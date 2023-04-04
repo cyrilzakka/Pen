2dc7fe996959565c5620e71576104bc33a73d824
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2023-04-03 21:38:07 -0700

    Initial Commit

---
+++ Pen/PenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenApp.swift
+//  Pen
+//
+//  Created by Cyril Zakka on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
